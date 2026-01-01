801b03e6b7ed7b198cd1768106ba346df4eeb213
Petar Nikolov <penikolov23@gmail.com>
2026-01-01 23:14:49 +0200

    Setup npm projects

--- notebooks/src/app.js
+++ notebooks/src/app.js
@@ -1,1 +1,11 @@
-console.log("Hello from notebooks!")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from notebooks!");
+});
+
+app.listen(3000, () => {
+  console.log("Notebooks service is running on port 3001");
+});
--- notes/src/app.js
+++ notes/src/app.js
@@ -1,1 +1,11 @@
-console.log("Hello from notes!")
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from notes!");
+});
+
+app.listen(3000, () => {
+  console.log("Notes service is running on port 3002");
+});
