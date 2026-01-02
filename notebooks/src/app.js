27be987928d5f442ef5733ab050600cd0d1e531f
Petar Nikolov <penikolov23@gmail.com>
2026-01-02 15:51:00 +0200

    Add health endpoint and healtcheck to

--- notebooks/src/app.js
+++ notebooks/src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const { healthRouter } = require("./routes/health");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use("/health", healthRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello from notebooks!");
 });
 
-app.listen(3000, () => {
-  console.log("Notebooks service is running on port 3001");
+app.listen(port, () => {
+  console.log(`Notebooks service is running on port ${port}`);
 });
